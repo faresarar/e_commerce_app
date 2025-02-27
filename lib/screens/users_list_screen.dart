9001dd3b28042c2e4fe1a39caa36900fe6eca5e8
faresarar <faresarar455@gmail.com>
2025-02-27 21:23:20 +0100

    complete users_list_screen.dart

--- lib/screens/users_list_screen.dart
+++ lib/screens/users_list_screen.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+import '../services/eccommerce_service.dart';
+import '../widgets/custom_app_bar.dart';
+import '../widgets/users_card.dart';
+import '../widgets/users_solid_card.dart';
+import 'user_profile_screen.dart';
 
 class UsersListScreen extends StatefulWidget {
   const UsersListScreen({super.key});
@@ -8,10 +15,57 @@ class UsersListScreen extends StatefulWidget {
 }
 
 class _UsersListScreenState extends State<UsersListScreen> {
+  List<dynamic> users = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUsers();
+  }
+
+  Future<void> _fetchUsers() async {
+    users = await EcommerceServices().fetchUsers();
+    setState(() {
+      users;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-      return Center(
-      child: Text("Users List"),
+    return Scaffold(
+      appBar: const CustomAppBar(title: "Users"),
+      body: users.isEmpty
+          ? Skeletonizer(
+              enabled: users.isEmpty,
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, crossAxisSpacing: 8, mainAxisSpacing: 8),
+                itemBuilder: (context, index) {
+                  return const UsersSolidCard();
+                },
+              ),
+            )
+          : GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, crossAxisSpacing: 8, mainAxisSpacing: 8),
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => UserProfile(
+                          user: users[index],
+                        ),
+                      ),
+                    );
+                  },
+                  child: UsersCard(
+                    user: users[index],
+                  ),
+                );
+              },
+            ),
     );
   }
 }
